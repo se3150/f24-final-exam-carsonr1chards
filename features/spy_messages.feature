70efec910689b18738c06a9a40d889b675870c48
carsonr1chards <carson.richards@utahtech.edu>
2024-12-09 13:46:31 -0700

    Implemented tests

--- features/spy_messages.feature
+++ features/spy_messages.feature
@@ -5,8 +5,18 @@ So that I can chat with my spy friends like a pro.
 
 Scenario: I can successfully encode a secret message
     Given I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"
-    # write your steps here
+    When I add "Testing the encode functionality" to the inputfield "#letters"
+    And I add "3" to the inputfield "#shift-amount"
+    And I click on the element "#submit"
+    And I pause for 200ms
+    Then I expect that element "#decoded_message p" contains the text "Whvwlqj wkh hqfrgh ixqfwlrqdolwb"
+
 
 Scenario: I can successfully decode a secret message
     Given I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"
-    # write your steps here
+    When I add "Decode" to the inputfield "#decoder-setting"
+    And I add "3" to the inputfield "#shift-amount"
+    And I add "Whvwlqj wkh hqfrgh ixqfwlrqdolwb" to the inputfield "#letters"
+    And I click on the element "#submit"
+    And I pause for 200ms
+    Then I expect that element "#decoded_message p" contains the text "Testing the encode functionality"
--- test_brute.py
+++ test_brute.py
@@ -1,5 +1,6 @@
 import pytest
 from brute import Brute
+from unittest.mock import Mock
 
 todo = pytest.mark.skip(reason='todo: pending spec')
 
@@ -11,8 +12,63 @@ def describe_Brute():
 
     def describe_bruteOnce():
         # write your test cases here
-        pass
+        
+        def it_successfully_compares_attempt_to_target():
+            b = Brute("testing")
+
+            assert b.bruteOnce("testing")
+
+
+        def it_fails_compare_attempt_to_target():
+            b = Brute("testing")
+
+            assert b.bruteOnce("wrong") == False
+
+        def it_fails_compare_int_to_string():
+            b = Brute('123456')
+
+            try:
+                b.bruteOnce(123456)
+                assert False
+            except TypeError as e:
+                assert True
+
+        def it_fails_no_attempt_param():
+            b = Brute("Missing parameter")
+
+            try:
+                b.bruteOnce()
+                assert False
+            except TypeError as e:
+                assert True
+
+        def it_fails_compare_with_and_without_spaces():
+            b = Brute("Secret String With Spaces")
+
+            assert b.bruteOnce("SecretStringWithSpaces") == False
 
     def describe_bruteMany():
         # write your test cases here
-        pass
+        
+        def it_cracks_target_small():
+            b = Brute("ab")
+
+            assert b.bruteMany() != -1
+
+        def it_cracks_target_large(mocker):
+            b = Brute("testingwithmocker")
+
+            mock_random_guess = mocker.patch.object(Brute, "randomGuess", return_value = "testingwithmocker")
+
+            assert b.bruteMany() != -1
+            mock_random_guess.assert_called_once_with() 
+
+        def it_fails_to_crack(mocker):
+            b = Brute("testingwithmocker")
+
+            mock_brute_many = mocker.patch.object(Brute, "bruteMany", return_value = -1)
+
+            assert b.bruteMany() == -1
+
+
+    
